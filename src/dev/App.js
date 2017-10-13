996aa1285bb0d2b2702f5be83259735739358559
Wayne Bloss <waynebloss@gmail.com>
2017-10-12 20:37:38 -0400

    Reorganize files.

--- src/dev/App.js
+++ src/dev/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import MyComponent from './lib/components/MyComponent';
+import MyComponent from '../lib/components/MyComponent';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './dev/index.css';
+import App from './dev/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
