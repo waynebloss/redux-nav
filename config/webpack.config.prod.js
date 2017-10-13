a37b81779cc74643c5da092a1b6431853a22d526
Wayne Bloss <waynebloss@gmail.com>
2017-10-12 20:01:27 -0400

    Maybe use backticks? It still gives error Module not found: Can't resolve 'style'...

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -168,8 +168,8 @@ module.exports = {
           {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract(
-              'style',
-              'css?importLoaders=1&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
+              `style`,
+              `css?importLoaders=1&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss`,
               extractTextPluginOptions
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
